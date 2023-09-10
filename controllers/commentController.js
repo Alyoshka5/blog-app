8817a30d20f845ac9e5160881fdb7adbaa5ed43c
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-10 16:15:03 -0700

    Add read list method

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,7 +2,9 @@ const asyncHandler = require('express-async-handler');
 const Comment = require('../models/comment');
 
 exports.readList = asyncHandler(async (req, res, next) => {
-    res.json();
+    const comments = await Comment.find({ post: req.params.postId }).sort({ datePosted: -1 }).exec();
+
+    res.json({ comments });
 });
 
 exports.create = asyncHandler(async (req, res, next) => {
