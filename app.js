4aa674528cbdc6b650ab9a547aec848b86178ad2
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-10 09:51:09 -0700

    Set up database connection

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const { mainModule } = require('process');
 
 const app = express();
+
+// database setup
+mongoose.set('strictQuery', false);
+const mongoDb = process.env.MONGO_URL
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDb);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
