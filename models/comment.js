93c9c09537f60bc35f60d68379130094143ade04
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-10 10:46:05 -0700

    Add comment model

---
+++ models/comment.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { format } = require('timeago.js');
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    content: { type: String, required: true, minLength: 1, maxLength: 1000 },
+    datePosted: { type: String, required: true, default: Date.now }
+});
+
+mongoose.virual('postedAgo').get(function() {
+    return format(this.datePosted);
+})
+
+module.exports = mognoose.model('Comment', CommentSchema);
