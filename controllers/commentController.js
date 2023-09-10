7f6477032b616e769d439d34df1824621d5c5982
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-10 16:53:41 -0700

    Add create handler method

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
 const Comment = require('../models/comment');
 
 exports.readList = asyncHandler(async (req, res, next) => {
@@ -7,6 +9,29 @@ exports.readList = asyncHandler(async (req, res, next) => {
     res.json({ comments });
 });
 
-exports.create = asyncHandler(async (req, res, next) => {
-    res.json();
-});
+exports.create = [
+    body('content')
+        .trim()
+        .isLength({ min: 1 }).withMessage('Comment must contain at least one character')
+        .isLength({ max: 1000 }).withMessage('Comment cannot contain more than 1000 characters'),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const comment = new Comment({
+            post: req.params.postId,
+            content: req.body.content
+        });
+
+        if (!errors.isEmpty()) {
+            res.status(400).json({
+                comment,
+                errors
+            });
+            return;
+        }
+
+        await comment.save();
+        res.status(200).send('success');
+    })
+]
