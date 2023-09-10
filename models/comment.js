49c73af79fe8305272cf64f9cfcbfc4b2fc26332
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-10 12:18:02 -0700

    Add post field to schema

--- models/comment.js
+++ models/comment.js
@@ -4,6 +4,7 @@ const { format } = require('timeago.js');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
+    post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
     content: { type: String, required: true, minLength: 1, maxLength: 1000 },
     datePosted: { type: String, required: true, default: Date.now }
 });
